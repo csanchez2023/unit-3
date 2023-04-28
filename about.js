0d51bf356edf05aa1732531af7b3bfa3c5fd8a33
Craig Sanchez <sanchezcraig32@gmail.com>
2023-04-27 23:00:12 -0600

    second commit unit 3

--- about.js
+++ about.js
@@ -4,11 +4,19 @@ console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
+	alert("Thank you for your feedback.")
 	
-	console.log('form submit');
+	// console.log('form submit');
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+function compliment(){
+	
+ document.getElementById("compliment") 
+ alert("Have a great day!!!");
+}
+
