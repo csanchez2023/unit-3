ae3244394bea49c9ba54634ea1a52175f9051c4d
Craig Sanchez <sanchezcraig32@gmail.com>
2023-05-09 21:38:49 -0600

    profile.js done and css button sylize complete

---
+++ profile.js
@@ -0,0 +1,13 @@
+document.getElementById ("color").addEventListener("click", function(){
+    alert("My Favorite Color is Blue")
+});
+
+
+document.getElementById ("place").addEventListener("click", function(){
+    alert("In my duck boat with a shotgun in hand and my best friends next to")
+});
+
+
+document.getElementById ("ritual").addEventListener("click", function(){
+    alert("Drink a cold can of pepsi after work!!")
+});
